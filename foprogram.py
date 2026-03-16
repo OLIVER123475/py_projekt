ee6b0d6d350890e39b8683e6aa9ee518b009633f
OLIVER123475 <oliver.burgermeister.au@gmail.com>
2026-03-16 14:03:34 +0100

    Add files via upload

---
+++ foprogram.py
@@ -0,0 +1,15 @@
+import menu
+
+import vasarlok
+
+import raktar
+
+import recept
+
+#int(input("Adja meg az asztalok szamat"))
+
+asztalok = [[], [], [], [], [], [], [], [], [], [], [], []]
+
+
+int(input("Válasszon asztalt. Adja meg az asztal számát:"))
+
